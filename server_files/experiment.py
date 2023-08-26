523e47d68a4d2ed9311d3a3d145d321cda86a9d3
Ronen Margolin <roni948576@gmail.com>
2023-08-26 16:10:33 +0300

    working code actually because git sucks

--- server_files/experiment.py
+++ server_files/experiment.py
@@ -15,7 +15,7 @@ from sklearn.preprocessing import normalize
 from sklearn.ensemble import GradientBoostingClassifier 
 from matplotlib import pyplot as plt
 from fields_and_encodings import Fields, Encodings
-from type_functions import TypeFunctions, ColumnCleaner    , ColumnCleaner    
+from type_functions import TypeFunctions, ColumnCleaner    
 
 # hyper params
 # hyper params
@@ -24,12 +24,8 @@ from type_functions import TypeFunctions, ColumnCleaner    , ColumnCleaner
 def output_to_log_and_terminal(output, level=logging.INFO):
     print(output)
     logging.log(level, output)
-def output_to_log_and_terminal(output, level=logging.INFO):
-    print(output)
-    logging.log(level, output)
 
 
-def table_columns(field_names):
 def table_columns(field_names):
     positive = pd.read_csv('csv/instance_0_positive.csv',nrows=1)
     selected_cols = [
@@ -43,9 +39,8 @@ def table_columns(field_names):
 
 def create_mixed_dataset(selected_cols):
     row_num = 0
-    row_num = 0
-    positive = pd.read_csv('csv/instance_0_positive.csv',usecols=selected_cols,  chunksize=1000,low_memory=False)
-    negative = pd.read_csv('csv/all_instance_negative.csv',usecols=selected_cols,  chunksize=1000,low_memory=False)
+    positive = pd.read_csv('csv/instance_0_positive.csv',usecols=selected_cols, chunksize=1000,low_memory=False)
+    negative = pd.read_csv('csv/all_instance_negative.csv',usecols=selected_cols, chunksize=1000,low_memory=False)
 
     cols = positive.get_chunk(0).columns
     print(len(cols))
@@ -54,14 +49,11 @@ def create_mixed_dataset(selected_cols):
     for p_chunk, n_chunk in tqdm.tqdm(zip(positive,negative), total=11):
         s_chunk = pd.concat([p_chunk,n_chunk]).sample(frac=1)
         row_num += s_chunk.shape[0]
-        row_num += s_chunk.shape[0]
         s_chunk[cols].to_csv('csv/shuffled_dataset_first_itter.csv',mode='a',header=False,index=False)
     del positive,negative, p_chunk, s_chunk, n_chunk
     return row_num
-    return row_num
 
 
-def map_fields_to_cleaners(field_names, field_types, field_encodings, selected_cols, enc_dict):
 def map_fields_to_cleaners(field_names, field_types, field_encodings, selected_cols, enc_dict):
     field_groups = {}
     for f, t, e in tqdm.tqdm(zip(field_names,field_types,field_encodings), total=len(field_names)):
@@ -69,23 +61,12 @@ def map_fields_to_cleaners(field_names, field_types, field_encodings, selected_c
         if f != 6152:
             res = []
             f = str(f)
-            for col in selected_selected_cols:
+            for col in selected_cols:
                 if col.startswith(f + '-'):
                     res.append(col)
             field_groups[f] = ColumnCleaner(res, t, enc_dict.get(e, []), f)
     return list(field_groups.values())
-            field_groups[f] = ColumnCleaner(res, t, enc_dict.get(e, []), f)
-    return list(field_groups.values())
 
-def activate_cleaners(dataset, cleaners, selected_cols, train_size):
-    ######
-    # Added the 6152 here 
-    # there should be a better way to add the 6152 but fuck it
-    y_cols = []
-    for col in selected_cols:
-            if col.startswith('6152-'):
-                y_cols.append(col)
-    ######
 def activate_cleaners(dataset, cleaners, selected_cols, train_size):
     ######
     # Added the 6152 here 
@@ -102,13 +83,8 @@ def activate_cleaners(dataset, cleaners, selected_cols, train_size):
         for cleaner in tqdm.tqdm(cleaners,desc=f"chunk {i+1}/1"):
             clean_data = cleaner.fit_transform(chunk[cleaner.column_names], train_size)
             chunk.drop(columns=cleaner.column_names, inplace=True)
-        for cleaner in tqdm.tqdm(cleaners,desc=f"chunk {i+1}/1"):
-            clean_data = cleaner.fit_transform(chunk[cleaner.column_names], train_size)
-            chunk.drop(columns=cleaner.column_names, inplace=True)
             if not clean_data.empty:
                 res_cols.append(clean_data)
-
-        res_cols.append(chunk[y_cols])
 
         res_cols.append(chunk[y_cols])
         chunk = pd.concat(res_cols,axis=1)
@@ -152,56 +128,12 @@ def prepare_data(exp_name):
     cleaners = map_fields_to_cleaners(field_names, field_types, field_encodings, selected_cols, enc_dict)
 
     output_to_log_and_terminal("done with type matching")
-        chunk.to_csv('csv/shuffled_dataset_clean.csv',mode='w',header=True,index=False)
-    return chunk.dtypes
-
-
-
-def prepare_data(exp_name):
-    # get desired field list
-    field_names, field_types, field_encodings = Fields.load_desired_fields()
-
-    output_to_log_and_terminal("loaded desired fields")
-
-    # build final field list including instances and arrays...
-    selected_cols = table_columns(field_names)
-
-    output_to_log_and_terminal("loaded resulting table columns")
-
-    # build final dataset from positive and negative with only needed columns
-    row_num = create_mixed_dataset(selected_cols)
-
-    # claculate train and test set seperation point
-    # the dataset is already shuffled so the seperation point is constant
-    train_size = row_num * 7 // 10
-
-    output_to_log_and_terminal(
-        "created mixed dataset, row count: " + 
-        str(row_num) + 
-        " train size: " + 
-        str(train_size)
-    )
-
-    # create a clean dataset with clean datatypes
-    dataset = pd.read_csv('csv/shuffled_dataset_first_itter.csv',chunksize=row_num, low_memory=False)
-
-    # build dict of encoding values
-    enc_dict = Encodings.create_category_dictionary()
-
-    # build final field to type dictionary
-    cleaners = map_fields_to_cleaners(field_names, field_types, field_encodings, selected_cols, enc_dict)
-
-    output_to_log_and_terminal("done with type matching")
 
     type_list = activate_cleaners(dataset,cleaners,selected_cols,train_size)
-    type_list = activate_cleaners(dataset,cleaners,selected_cols,train_size)
 
-    output_to_log_and_terminal("shuffled_dataset_clean.csv is created")
     output_to_log_and_terminal("shuffled_dataset_clean.csv is created")
 
     # if all works this will print only number and float dtypes
-    print(np.unique(type_list).tolist())
-    return cleaners
     print(np.unique(type_list).tolist())
     return cleaners
 
@@ -220,9 +152,7 @@ def load_data(exp_name):
     
     cleaner_list = np.load(exp_name+"_cleaner_list.npy", allow_pickle=True)
 
-    cleaner_list = np.load(exp_name+"_cleaner_list.npy", allow_pickle=True)
-
-    return X,Y, x_cols, cleaner_list, cleaner_list
+    return X,Y, x_cols, cleaner_list
 
 def experiment(exp_name,remake_dataset):
     #####
@@ -236,28 +166,17 @@ def experiment(exp_name,remake_dataset):
     output_to_log_and_terminal("start: " + str(datetime.datetime.now()))
 
     cleaner_list = []
-    
-    output_to_log_and_terminal("start: " + str(datetime.datetime.now()))
-
-    cleaner_list = []
     if remake_dataset:
         output_to_log_and_terminal("remake dataset")
         cleaner_list = prepare_data(exp_name)
         np.save(exp_name+"_cleaner_list",cleaner_list,allow_pickle=True)
     X,Y, x_cols, cleaner_list = load_data(exp_name)
-        output_to_log_and_terminal("remake dataset")
-        cleaner_list = prepare_data(exp_name)
-        np.save(exp_name+"_cleaner_list",cleaner_list,allow_pickle=True)
-    X,Y, x_cols, cleaner_list = load_data(exp_name)
 
-    output_to_log_and_terminal("loaded data")
     output_to_log_and_terminal("loaded data")
     clf = LogisticRegression(penalty='l1',solver='liblinear',max_iter=200,verbose=True)
     train_limit = X.shape[0]*7//10
     output_to_log_and_terminal("before fit")
-    output_to_log_and_terminal("before fit")
     clf.fit(X[:train_limit],Y[:train_limit])
-    output_to_log_and_terminal("after fit")
     output_to_log_and_terminal("after fit")
 
     test_score = clf.score(X[train_limit:],Y[train_limit:])
